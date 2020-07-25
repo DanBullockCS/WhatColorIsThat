00f5d35fb6ba1e97bcb309ad34ac9b5f73fe739c
Dan <businessdaniel@live.com>
2020-07-25 16:19:03 -0400

    Added crosshair-selection & some styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,36 @@
-var canvas, ctx, fileinput, container, img,
+var canvas, backcan, ctx, fileinput, container, img, rgbColorCode, hexColorCode
     prevX = 0,
     prevY = 0,
     currX = 0,
     currY = 0;
 
-function init() {
+$(document).ready(function () {
     canvas = document.getElementById('can');
+    backcan = document.getElementById('backcan');
     ctx = canvas.getContext("2d");
-    input = document.getElementById('imageLoader');
+    backctx = backcan.getContext("2d");
     container = document.getElementById('canvasContainer');
     img = new Image();
-    colorCode = document.getElementById('colorCode');
+    rgbColorCode = document.getElementById('rgbColorCode');
+    hexColorCode = document.getElementById('hexColorCode');
 
-    canvas.addEventListener("mousemove", function (e) {
+    // Event Listeners
+    $('#can').on("mousemove", function (e) {
         findxy('move', e);
     });
+    
+    $('#can').on('mousedown touchstart', MouseDown);
+    $('#can').on('mousedown touchstart', DragMouse);
 
-    canvas.addEventListener("click", function (e) {
-        getRGBValue();
+    $('#can').on('mouseup touchend', function () {
+        $('#can').off('mousemove touchmove', DragMouse);
+        $('#can').off('mousemove touchmove', getRGBValue);
     });
 
-    input.addEventListener("change", function (e) {
+    $('#imageLoader').on("change", function (e) {
         showUploadedImage(e);
     });
-}
+});
 
 function findxy(res, e) {
     if (res == 'move') {
@@ -32,6 +39,42 @@ function findxy(res, e) {
         currX = e.clientX - container.offsetLeft;
         currY = e.clientY - container.offsetTop;
     }
+}
+
+function MouseDown() {
+    // While the mouse is down, call these functions
+    $('#can').on('mousemove touchmove', DragMouse);
+    $('#can').on('mousemove touchmove', getRGBValue);
+}
+
+function DragMouse(e) {
+    // While the mouse is being dragged, draw the crosshair
+    var mx = currX;
+    var my = currY;
+
+    if (isNaN(mx)) {
+        mx = e.originalEvent.touches[0].pageX;
+        my = e.originalEvent.touches[0].pageY;
+    }
+
+    drawCrossHair(mx, my);
+}
+
+function drawCrossHair(x, y) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // H 
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.moveTo(0, y - 1);
+    ctx.lineTo(500, y + 1);
+    ctx.stroke();
+
+    // V
+    ctx.beginPath();
+    ctx.moveTo(x - 1, 0);
+    ctx.lineTo(x + 1, 500);
+    ctx.stroke();
 }
 
 function showUploadedImage(evt) {
@@ -46,19 +89,19 @@ function showUploadedImage(evt) {
         reader.onload = function (evt) {
             if (evt.target.readyState == FileReader.DONE) {
                 // Get image from uploaded file  
-                img.src = evt.target.result; 
+                img.src = evt.target.result;
                 // Clear the canvas
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                backctx.clearRect(0, 0, canvas.width, canvas.height);
 
                 // Make sure the img is loaded before 
-                img.onload = function() {
+                img.onload = function () {
                     // Calculate the scale of the canvas and image
                     var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
                     var x = (canvas.width / 2) - (img.width / 2) * scale;
                     var y = (canvas.height / 2) - (img.height / 2) * scale;
-                    
+
                     // Draw the image on the canvas
-                    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+                    backctx.drawImage(img, x, y, img.width * scale, img.height * scale);
                 }
             }
         }
@@ -69,7 +112,37 @@ function showUploadedImage(evt) {
 }
 
 function getRGBValue() {
-    let imagedata = ctx.getImageData(currX, currY, canvas.width, canvas.height).data;
-    let rgb = [ imagedata[0], imagedata[1], imagedata[2] ];
-    colorCode.value = rgb;
+    let imagedata = backctx.getImageData(currX, currY, canvas.width, canvas.height).data;
+    let rgb = [imagedata[0], imagedata[1], imagedata[2]];
+
+    let hex = [];
+    let resultHex = "";
+    for (let i = 0; i < 3; i++) {
+        hex.push(Number(rgb[i]).toString(16));
+        if (hex[i].length < 2) {
+            hex[i] = "0" + hex[i];
+        }
+
+        // Build the resulting string
+        resultHex += hex[i];
+    }
+
+    rgbColorCode.value = rgb;
+    hexColorCode.value = resultHex;
 }
+
+// Prevent default code for touchscreens
+window.blockMenuHeaderScroll = false;
+$(window).on('touchstart', function (e) {
+    if ($(e.target).closest('#can').length == 1) {
+        blockMenuHeaderScroll = true;
+    }
+});
+$(window).on('touchend', function () {
+    blockMenuHeaderScroll = false;
+});
+$(window).on('touchmove', function (e) {
+    if (blockMenuHeaderScroll) {
+        e.preventDefault();
+    }
+});
