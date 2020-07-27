9be8244f18f9797379fc8a075f477b49bfc2fb33
Dan <businessdaniel@live.com>
2020-07-27 16:24:05 -0400

    Darkmode and favicon

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,25 @@
 var canvas, backcan, colorcan, ctx, backctx, colorctx, fileinput, container, img, rgbColorCode, hexColorCode
-    prevX = 0,
+prevX = 0,
     prevY = 0,
     currX = 0,
     currY = 0;
 
 $(document).ready(function () {
+    // Darkmode
+    $("body").toggleClass(localStorage.toggled);
+    $("body").hasClass('dark-mode') ? $('#dark-mode-switch').prop("checked", true) : $('#dark-mode-switch').prop("checked", false);
+
     // Select canvas & div containing them
     canvas = document.getElementById('can');
     backcan = document.getElementById('backcan');
     colorcan = document.getElementById('colorcan');
     container = document.getElementById('canvasContainer');
-    
+
     // Get Canvas Contexts
     ctx = canvas.getContext("2d");
     backctx = backcan.getContext("2d");
     colorctx = colorcan.getContext("2d");
-    
+
     // Setup Inputs & the uploaded Image
     rgbColorCode = document.getElementById('rgbColorCode');
     hexColorCode = document.getElementById('hexColorCode');
@@ -26,7 +30,7 @@ $(document).ready(function () {
     $('#can').on("mousemove", function (e) {
         findxy('move', e);
     });
-    
+
     $('#can').on('mousedown touchstart', MouseDown);
     $('#can').on('mousedown touchstart', DragMouse);
 
@@ -91,7 +95,7 @@ function drawCurrColor() {
 
     colorctx.stroke();
     colorctx.beginPath();
-    colorctx.rect(0, 0, 200, canvas.width);
+    colorctx.rect(0, 0, colorcan.width, colorcan.height);
     colorctx.fillStyle = "#" + hexColorCode.value;
     colorctx.fill();
 }
@@ -141,31 +145,37 @@ function getRGBValue() {
         if (hex[i].length < 2) {
             hex[i] = "0" + hex[i];
         }
-
-        // Build the resulting string
-        resultHex += hex[i];
+        resultHex += hex[i]; // Build the resulting string
     }
 
+    // Display color values
     rgbColorCode.value = rgb;
     hexColorCode.value = resultHex;
 
     // Using ntc.js from Chirag Mehta
-    let NameThatColorArray  = ntc.name("#"+resultHex);
+    let NameThatColorArray = ntc.name("#" + resultHex);
     colorName.value = NameThatColorArray[1];
 }
 
 // Prevent default code for touchscreens
 window.blockMenuHeaderScroll = false;
 $(window).on('touchstart', function (e) {
-    if ($(e.target).closest('#can').length == 1) {
-        blockMenuHeaderScroll = true;
-    }
+    if ($(e.target).closest('#can').length == 1) { blockMenuHeaderScroll = true; }
 });
 $(window).on('touchend', function () {
     blockMenuHeaderScroll = false;
 });
 $(window).on('touchmove', function (e) {
-    if (blockMenuHeaderScroll) {
-        e.preventDefault();
-    }
+    if (blockMenuHeaderScroll) { e.preventDefault(); }
 });
+
+// Dark Mode Toggle
+function toggleDarkMode() {
+    if (localStorage.toggled != 'dark-mode') {
+        $("body").toggleClass('dark-mode', true);
+        localStorage.toggled = "dark-mode";
+    } else {
+        $("body").toggleClass('dark-mode', false);
+        localStorage.toggled = "";
+    }
+}
